426b4d8e8651c76022b2c4e71b405e81aa8e9d70
David Terra Alves <david@davidterra.com>
2020-12-30 13:20:47 -0300

    Adicionado evento para ser dispardo quando o produto estive abaixo do estoque.

--- src/Services/Catalogo/NerdStore.Catalogo.Domain/EstoqueService.cs
+++ src/Services/Catalogo/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading.Tasks;
+using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Bus;
 
 namespace NerdStore.Catalogo.Domain
 {
@@ -11,8 +13,14 @@ namespace NerdStore.Catalogo.Domain
     public class EstoqueService : IEstoqueService
     {
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IMediatrHandler _bus;
 
-        public EstoqueService(IProdutoRepository produtoRepository) => _produtoRepository = produtoRepository;
+        public EstoqueService(IProdutoRepository produtoRepository,
+            IMediatrHandler bus)
+        {
+            _bus = bus;
+            _produtoRepository = produtoRepository;
+        }
 
         public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
         {
@@ -24,16 +32,21 @@ namespace NerdStore.Catalogo.Domain
 
             produto.DebitarEstoque(quantidade);
 
+            // TODO: Parametrizar a quantidade de estoque baixo
+
+            if (produto.QuantidadeEstoque < 10)
+                await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produto.Id, produto.QuantidadeEstoque));
+
             _produtoRepository.Atualizar(produto);
 
             return await _produtoRepository.UnitOfWork.Commit();
-        }        
+        }
 
         public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
         {
             var produto = await _produtoRepository.ObterPorId(produtoId);
 
-            if(produto == null) return false;
+            if (produto == null) return false;
             produto.ReporEstoque(quantidade);
 
             _produtoRepository.Atualizar(produto);
